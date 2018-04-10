20d8bd9f8d418a1293222f399dbdbf0fcca30d64
Steve Milner <smilner@redhat.com>
2018-04-10 10:29:54 -0400

    examples: Update setup.sh for new example locations

--- examples/setup.sh
+++ examples/setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo "## Creating the name space..."
-oc create -f examples/namespace.yaml
+oc create -f examples/deploy/00-namespace.yaml
 echo "## Adding the cluster role..."
-oc create -f examples/cluster-role.yaml
+oc create -f examples/deploy/rbac/cluster-role.yaml
 echo "## Adding role bindings..."
-oc create -f examples/cluster-role-binding.yaml
+oc create -f examples/deploy/rbac/cluster-role-binding.yaml
 echo "## Adding the operator..."
-oc create -f examples/update-operator.yaml
+oc create -f examples/deploy/update-operator.yaml
 echo "## Adding the agent..."
-oc create -f examples/update-agent.yaml
+oc create -f examples/deploy/update-agent.yaml
 sleep 5
 echo "## Agent DS"
 oc get ds --namespace=reboot-coordinator
